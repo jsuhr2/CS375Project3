e8536858e8b4324e083356d28d657d0214ffb563
jsuhr2 <jsuhr2@binghamton.edu>
2018-10-22 15:59:45 -0400

    finished with reading of input

--- suhr_j_p3/program3.cpp
+++ suhr_j_p3/program3.cpp
@@ -27,7 +27,7 @@ Item::Item(double w, double v){
 
 class Knapsack{
 	public:
-		vector<Item> knapsack;
+		vector<Item> items;
 		Knapsack();
 		void sortByRatio();
 };
@@ -37,19 +37,33 @@ Knapsack::Knapsack(){
 
 int main(int argc, char** argv){
 	
+	Knapsack sack;
 	int numItems = 0;
 	int capacity = 0;
+	int tempWeight = 0;
+	int tempValue = 0;
 
+//clear output file
 	ofstream clear;
 	clear.open(argv[2], ofstream::out | ofstream::trunc);
 	clear.close();
 
+//open files
 	ifstream input(argv[1], ios::in);
 
 	ofstream output;
 	output.open(argv[2], ios_base::app);
 	
-	
+	input >> numItems;
+	input >> capacity;
+
+	while(true){
+		input >> tempWeight;
+		if(input.eof())
+			break;
+		input >> tempValue;
+		sack.items.push_back(Item(tempWeight, tempValue));
+	}
 	
 	input.close();
 	output.close();
